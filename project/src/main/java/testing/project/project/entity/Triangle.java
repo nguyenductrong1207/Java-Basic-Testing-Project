62b2c052512baeedb6c1ca15d59aedc4952fcc4d
nguyenductrong1207 <131521216+nguyenductrong1207@users.noreply.github.com>
2024-02-22 23:04:57 +0700

    fix error

--- project/src/main/java/testing/project/project/entity/Triangle.java
+++ project/src/main/java/testing/project/project/entity/Triangle.java
@@ -13,7 +13,6 @@ public class Triangle {
     private int side1;
     private int side2;
     private int side3;
-    private String TriangleType;
 
     public void setSide(int s1, int s2, int s3) {
         this.side1 = s1;
@@ -34,36 +33,34 @@ public class Triangle {
     }
 
     public boolean isTriangle() {
-        if ((side1 < side2 + side3) && (side2 < side1 + side3) && (side3 < side1 + side2)) {
+        if ((side1 < side2 + side3) && (side2 < side1 + side3) && (side3 < side2 + side1))
             return true;
-        } else {
+        else
             return false;
-        }
     }
 
     public String classify() {
-        boolean c1, c2, c3;
 
-        c1 = (1 <= side1) && (side1 <= 200);
-        c2 = (1 <= side2) && (side2 <= 200);
-        c3 = (1 <= side3) && (side3 <= 200);
+        boolean c1 = (1 <= side1) && (side1 <= 200);
+        boolean c2 = (1 <= side2) && (side2 <= 200);
+        boolean c3 = (1 <= side3) && (side3 <= 200);
 
         if (!c1 || !c2 || !c3) {
-            TriangleType = "OUT_OF_RANGE";
-        } else {
-            if (isTriangle()) {
-                if ((side1 == side2) && (side2 == side3) && (side3 == side1)) {
-                    TriangleType = "EQUILATERAL";
-                } else if ((side1 != side2) && (side1 != side3) && (side2 != side3)) {
-                    TriangleType = "SCALENE";
-                } else {
-                    TriangleType = "ISOSCELES";
-                }
-            } else {
-                TriangleType = "IsNotTriangle";
-            }
+            return "OUT_OF_RANGE";
         }
-        return TriangleType;
+
+        if (isTriangle()) {
+
+            if ((side1 == side2) && (side2 == side3))
+                return "EQUILATERAL";
+
+            else if ((side1 != side2) && (side1 != side3) && (side2 != side3))
+                return "SCALENE";
+
+            else
+                return "ISOSCELES";
+        }
+        return "IsNotTriangle";
     }
 
 }
